2428accb88043472bdf90e0c4fab6018f4c73525
Karen Jo <karenfiksdahl@gmail.com>
2024-02-20 23:14:18 +0100

    creates new pages folder, moves all pages files to new path. Sets up Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import ProductPage from "./pages/ProductPage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
+      <Router>
     <div className="App">
-      
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/" element={<ProductPage />} />
+        <Route exact path="/" element={<About />} />
+        <Route exact path="/" element={<Contact />} />
+        <Route exact path="/" element={<Cart />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
