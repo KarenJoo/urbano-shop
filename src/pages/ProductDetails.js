7aedbdaf476e82c78e3097096744bb47ca28b05a
Karen Jo <karenfiksdahl@gmail.com>
2024-02-22 19:41:25 +0100

    fetches and displays product review and discount price

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -9,7 +9,7 @@ export default function ProductDetails() {
   }
 
   if (error || !productData) {
-    return <div>Error: Unable to load product details. Please try again later.</div>;
+    return <div>Error: Unable to load product details.</div>;
   }
 
   const product = productData.data;
@@ -19,19 +19,40 @@ export default function ProductDetails() {
   }
 
   return (
-    <div className="product-details-card">
-      <div className="product-details-image">
-        <img src={product.image.url} alt={product.image.alt} />
-      </div>
-      <div className="product-details-content">
-        <div className='product-details-head'>
-          <h2>{product.title}</h2>
-          <p>{product.description}</p>
+    <div className="product-details-container">
+      <div className="product-details-card">
+        <div className="product-details-image">
+          <img src={product.image.url} alt={product.image.alt} />
         </div>
-        <div className='product-details-foot'>
-          <h3>{product.price} NOK</h3>
-          <button className="buy-button">Add to Cart</button>
-        </div>      
+        <div className="product-details-content">
+          <div className='product-details-head'>
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+          </div>
+          <div className='product-details-foot'>
+            <h5>Original Price: {product.price} NOK</h5>
+            <h3>On Sale: {product.discountedPrice} NOK</h3>
+            <button className="buy-button">Add to Cart</button>
+          </div>      
+        </div>
+      </div>
+      <h2>Reviews</h2>
+      <div className="reviews-container">
+        {product.reviews && product.reviews.length > 0 ? (
+          product.reviews.map(review => (
+            <div key={review.id} className="review">
+              <div className='review-head'>
+              <p>Rating: {review.rating}</p>
+              </div>
+              <div className='review-foot'>
+              <p><strong>{review.username}</strong></p>
+              <p>{review.description}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No reviews available.</p>
+        )}
       </div>
     </div>
   );
