1cdfe6105ff5dbc37d3e3526645740b00a34ba9b
Karen Jo <karenfiksdahl@gmail.com>
2024-02-27 22:12:21 +0100

    adds buttonstyles to productDetails page, adds cart icon to assets

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import useFetch from '../hooks/useFetch'
-import { PRODUCT_ID_URL } from '../utils/api'
-import styles from '../pages/ProductDetails.module.css'
+import React from 'react';
+import useFetch from '../hooks/useFetch';
+import { PRODUCT_ID_URL } from '../utils/api';
+import styles from '../pages/ProductDetails.module.css';
+import buttonStyles from '../components/Buttons.module.css';
 
 export default function ProductDetails() {
   const { data: productData, loading, error } = useFetch(`${PRODUCT_ID_URL}`)
@@ -34,7 +35,7 @@ export default function ProductDetails() {
           <div className={styles.productDetailsFoot}>
             <h4>{product.price} NOK</h4>
             <h3>Sale: {product.discountedPrice} NOK</h3>
-            <button className={styles.buyButton}>Add to Cart</button>
+            <button className={buttonStyles.primaryButton}>Add to Cart</button>
           </div>
         </div>
       </div>
