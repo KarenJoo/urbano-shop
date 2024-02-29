39bde7c7f14d2de653351554b96930a694f5e637
Karen Jo <karenfiksdahl@gmail.com>
2024-02-29 15:55:35 +0100

    styles cart icon and add to cart counter in Navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,36 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Navbar.module.css';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Navbar.module.css'
+import { useSelector } from 'react-redux'
+import cartIcon from '../../assets/icons/shopping-cart.svg'
 
 export default function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const cartCount = useSelector((state) => state.cart.value); // Corrected to state.cart.value
+  const [menuOpen, setMenuOpen] = useState(false)
+  const cartCount = useSelector((state) => state.cart.value)
 
   const dropDownMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
+    setMenuOpen(!menuOpen)
+  }
 
   useEffect(() => {
     const handleMenuClick = () => {
-      const burgerMenu = document.querySelector(`.${styles.burgerMenu}`);
-      burgerMenu.addEventListener('click', function() {
-        this.classList.toggle(styles.active);
-      });
-    };
+      const burgerMenu = document.querySelector(`.${styles.burgerMenu}`)
+      burgerMenu.addEventListener('click', function () {
+        this.classList.toggle(styles.active)
+      })
+    }
 
-    handleMenuClick();
+    handleMenuClick()
 
     return () => {
-      const burgerMenu = document.querySelector(`.${styles.burgerMenu}`);
-      burgerMenu.removeEventListener('click', handleMenuClick);
-    };
-  }, [menuOpen]);
+      const burgerMenu = document.querySelector(`.${styles.burgerMenu}`)
+      burgerMenu.removeEventListener('click', handleMenuClick)
+    }
+  }, [menuOpen])
 
   return (
     <nav className={styles.navbar}>
       <h1>Urbano</h1>
-      <button className={styles.burgerMenu} onClick={dropDownMenu} aria-label='Toggle navigation'>
+      <button
+        className={styles.burgerMenu}
+        onClick={dropDownMenu}
+        aria-label='Toggle navigation'
+      >
         <span></span>
         <span></span>
         <span></span>
@@ -39,11 +44,14 @@ export default function Navbar() {
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
         <Link to='/contact'>Contact</Link>
-        <Link to='/cart'>Cart</Link>
+        <Link to='/cart' className={styles.cart}>
+          {' '}
+          <img className={styles.cartIcon} src={cartIcon} alt='Cart' />{' '}
+        </Link>
         <div className={styles.cartCount}>
           <p>{cartCount}</p>
         </div>
       </div>
     </nav>
-  );
+  )
 }
