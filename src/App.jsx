8621eadd4d91ead6732856dd5308f02cd573f62d
Karen Jo <karenfiksdahl@gmail.com>
2024-03-01 09:31:28 +0100

    removes  global styles and imports app styles to Grommet theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Grommet } from 'grommet'
 import Homepage from './pages/Homepage'
 import ProductDetails from './pages/ProductDetails'
 import About from './pages/About'
@@ -13,8 +14,35 @@ import Layout from './components/Layout'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
 
+const theme = {
+  global: {
+    font: {
+      family: 'Lexend, sans-serif',
+      size: '18px', 
+      height: '20px',
+    },
+    colors: {
+      brand: '#38609d',
+      background: '#d6d7db',
+      'accent-1': '#9ca9e0',
+      focus: '#38609d',
+      border: {
+        light: '#888888',
+      },
+      dark: '#000',
+      'custom-hover': '#38609d', 
+    },
+    elevation: {
+      light: {
+        small: '0 2px 2px #888888', 
+      },
+    },
+  },
+};
+
 function App() {
   return (
+    <Grommet theme={theme} full style={{ backgroundImage: "url('/assets/images/bg.jpg')", backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover', backgroundAttachment: 'fixed' }}>
     <Provider store={store}>
       <Router>
         <div className='App'>
@@ -32,6 +60,7 @@ function App() {
         </div>
       </Router>
     </Provider>
+    </Grommet>
   )
 }
 
