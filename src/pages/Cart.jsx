8ee535587fe611a55830f80dc1e6210ead1d6bfe
Karen Jo <karenfiksdahl@gmail.com>
2024-03-04 17:45:50 +0100

    styles cart cards in global stylesheet

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-
+import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function CartPage() {
-
+  const cartItems = useSelector((state) => state.cart.cartItems)
 
   return (
-    <div className='cart-container'>
-      <h2>Cart</h2>
-     
+    <div className='productContainer'>
+      <div className='cartContainer'>
+        <h2>Cart</h2>
+        {cartItems.map((item) => (
+          <div className='cartCard' key={item.id}>
+            <img src={item.image.url} alt={item.image.alt} />
+            <div className='cartCardFoot'>
+              <p>{item.title}</p>
+              <p>{item.price} NOK</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
