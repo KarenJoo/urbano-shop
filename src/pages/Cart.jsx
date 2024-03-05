1d3111a7165211ab71598dcdf0fb452d06d95442
Karen Jo <karenfiksdahl@gmail.com>
2024-03-05 15:39:47 +0100

    adds remove item button on cart items, not yet responsive

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../store/cartSlice';
-
+import buttonStyles from '../components/Buttons.module.css'
 export default function CartPage() {
   const [cartItems, setCartItems] = useState([]);
   const dispatch = useDispatch();
@@ -17,15 +17,25 @@ export default function CartPage() {
     dispatch(addToCart(product));
     const savedCartItems = cartItems.findIndex((item) => item.id === product.id);
     if (savedCartItems !== -1) {
+
       const addedCartItems = [...cartItems];
       addedCartItems[savedCartItems].quantity++;
       setCartItems(addedCartItems);
       localStorage.setItem('cartItems', JSON.stringify(addedCartItems));
+
     } else {
       setCartItems([...cartItems, { ...product, quantity: 1 }]);
       localStorage.setItem('cartItems', JSON.stringify([...cartItems, { ...product, quantity: 1 }]));
     }
   };
+
+  const removeCartItem = (index) => {
+    const removedCartItem = [...cartItems];
+    removedCartItem.splice(index, 1);
+    setCartItems(removedCartItem);
+    localStorage.setItem('cartItems', JSON.stringify(removedCartItem));
+  };
+  
 
   return (
     <div className='productContainer'>
@@ -37,8 +47,11 @@ export default function CartPage() {
           <div className='cartCardContent'>
             <p>{item.title}</p>
             <p>Price: {item.price} NOK</p>
-            <p>Quantity: {item.quantity}</p>
+            <p>Quantity: {item.quantity}</p>         
+             <button className={buttonStyles.primaryButton} onClick={() => removeCartItem(index)}>Remove item</button>
+
           </div>
+
         </div>
       ))}
     </div>
