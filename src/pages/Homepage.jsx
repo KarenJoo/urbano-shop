d6159fb780cb98117854e8ac3fae9c793d93f291
Karen Jo <karenfiksdahl@gmail.com>
2024-03-08 00:37:02 +0100

    responsive search products filters products including searchTerm

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,14 +5,12 @@ import { PRODUCTS_URL } from '../utils/api'
 import styles from './Homepage.module.css'
 import buttonStyles from '../components/Buttons.module.css'
 import navbarStyles from '../components/Header/Navbar.module.css'
-import { TextInput, Box, Button } from "grommet";
-
+import { TextInput, Box, Button } from 'grommet'
 
 export default function Homepage() {
-  const [displayCount, setDisplayCount] = useState(6);
-  const [searchTerm, setSearchTerm] = useState(''); 
-  const [filter, setFilter] = useState('');
-  const { data: productData, loading, error } = useFetch(`${PRODUCTS_URL}`);
+  const [displayCount, setDisplayCount] = useState(6)
+  const [searchTerm, setSearchTerm] = useState('')
+  const { data: productData, loading, error } = useFetch(`${PRODUCTS_URL}`)
 
   if (loading) {
     return <div>Loading...</div>
@@ -27,52 +25,44 @@ export default function Homepage() {
   const handleViewMore = () => {
     setDisplayCount((prevCount) => prevCount + 6)
   }
-
-  const handleFilter = (filterValue) => { 
-    setFilter(filterValue);
-  };
-
+  // Filter products based on search term
   const filteredProducts = products.filter((product) =>
-  product.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
-  (filter ? product.discountedPrice === filter : true)
-);
+    product.title.toLowerCase().includes(searchTerm.toLowerCase()),
+  )
 
   return (
     <>
-    <div className="productContainer">
-
-    <Box pad="small">
-      <TextInput
-        placeholder="Search product..."
-        value={searchTerm}
-        onChange={(event) => setSearchTerm(event.target.value)}
-      />
-      <Box direction="row" gap="small" margin={{ top: 'small' }}>
-        <Button label="All" onClick={() => handleFilter('')} />
-        <Button label="Sale" onClick={() => handleFilter('Category1')} />
-      </Box>
-    </Box>
-
-      <h2 className={styles.headerText}>Shop Urbano</h2>
-      <div className={styles.productList}>
-        {products && products.slice(0, displayCount).map((product) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            title={product.title}
-            description={product.description}
-            price={product.price}
-            discountedPrice={product.discountedPrice}
-            img={product.image.url}
+      <div className='productContainer'>
+        <Box pad='medium'>
+          <TextInput
+            placeholder='Search products...'
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
           />
-        ))}
+        </Box>
+        <h2 className={styles.headerText}>Shop Urbano</h2>
+        <div className={styles.productList}>
+          {filteredProducts.slice(0, displayCount).map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+              title={product.title}
+              description={product.description}
+              price={product.price}
+              discountedPrice={product.discountedPrice}
+              img={product.image.url}
+            />
+          ))}
+        </div>
+        {filteredProducts.length > displayCount && (
+          <button
+            className={buttonStyles.primaryButton}
+            onClick={handleViewMore}
+          >
+            View More
+          </button>
+        )}
       </div>
-      {products.length > displayCount && (
-        <button className={buttonStyles.primaryButton} onClick={handleViewMore}>
-          View More
-        </button>
-      )}
-    </div>
-  </>
-);
+    </>
+  )
 }
