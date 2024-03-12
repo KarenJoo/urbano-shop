9d2e0bc76c4ef2910ddccdecb687791e9e1e2535
Karen Jo <karenfiksdahl@gmail.com>
2024-03-12 21:41:45 +0100

    styles cart icon and cartcount in small screens

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,38 +8,19 @@ export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
   const cartItems = useSelector((state) => state.cart.cartItems);
 
-  // Calculate the total quantity of all items in the cart for the initial render
-  const calculateTotalQuantity = () => {
-    return cartItems.reduce((total, item) => total + item.quantity, 0);
-  };
+  // Initialize cartCount state with the total quantity
+  const [cartCount, setCartCount] = useState(calculateTotalQuantity());
 
-  const [cartCount, setCartCount] = useState(() => {
-  
-  });
-  
+  function calculateTotalQuantity() {
+    return cartItems.reduce((total, item) => total + item.quantity, 0);
+  }
+
   const dropDownMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
   useEffect(() => {
-    const handleMenuClick = () => {
-      const burgerMenu = document.querySelector(`.${styles.burgerMenu}`);
-      burgerMenu.addEventListener('click', function () {
-        this.classList.toggle(styles.active);
-      });
-    };
-
-    handleMenuClick();
-
-    return () => {
-      const burgerMenu = document.querySelector(`.${styles.burgerMenu}`);
-      burgerMenu.removeEventListener('click', handleMenuClick);
-    };
-  }, [menuOpen]);
-
-  useEffect(() => {
-    // Update to calculate total quantity based on item quantities in the cart
-    const totalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
+    const totalQuantity = calculateTotalQuantity();
     setCartCount(totalQuantity);
     localStorage.setItem('cartCount', totalQuantity.toString()); // Update localStorage with the new total quantity
   }, [cartItems]);
@@ -47,26 +28,27 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <h1>Urbano</h1>
-      <button
-        className={styles.burgerMenu}
-        onClick={dropDownMenu}
-        aria-label='Toggle navigation'
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
+      <div className={styles.menuAndCartContainer}>
+        <button
+          className={styles.burgerMenu}
+          onClick={dropDownMenu}
+          aria-label='Toggle navigation'
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <Link to='/cart' className={styles.cart}>
+          <img className={styles.cartIcon} src={cartIcon} alt='Cart' />
+          <div className={styles.cartCount}>
+            <p>{cartCount}</p>
+          </div>
+        </Link>
+      </div>
       <div className={`${styles.navLinks} ${menuOpen ? styles.active : ''}`}>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
         <Link to='/contact'>Contact</Link>
-        <Link to='/cart' className={styles.cart}>
-          {' '}
-          <img className={styles.cartIcon} src={cartIcon} alt='Cart' />{' '}
-        </Link>
-        <div className={styles.cartCount}>
-          <p>{cartCount}</p>
-        </div>
       </div>
     </nav>
   );
