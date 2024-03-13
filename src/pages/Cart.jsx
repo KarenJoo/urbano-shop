32f6322a4633eaeadd15dfa4c9e66b615704c1e6
Karen Jo <karenfiksdahl@gmail.com>
2024-03-13 22:41:56 +0100

    styles cart item card, adds link to item id

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,45 +1,52 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { removeCartItem } from '../store/cartSlice';
-import buttonStyles from '../components/Buttons.module.css';
-import styles from './Cart.module.css';
-import { calculateCart } from '../store/cartCalculation';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeCartItem } from '../store/cartSlice'
+import buttonStyles from '../components/Buttons.module.css'
+import styles from './Cart.module.css'
+import { calculateCart } from '../store/cartCalculation'
+import { Link } from 'react-router-dom'
 
 export default function CartPage() {
-  const cartItems = useSelector((state) => state.cart.cartItems);
-  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart.cartItems)
+  const dispatch = useDispatch()
 
- 
-  const totalSum = calculateCart(cartItems);
-  const totalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
-
+  const totalSum = calculateCart(cartItems)
+  const totalQuantity = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0,
+  )
 
   const handleRemoveCartItem = (id) => {
-    dispatch(removeCartItem({ id }));
-  };
+    dispatch(removeCartItem({ id }))
+  }
 
   return (
-    <div className="productContainer">
+    <div className='productContainer'>
       <div className={styles.cartContainer}>
         <h2>Checkout page</h2>
         <h3>Products in cart ({totalQuantity})</h3>
         {cartItems.map((item) => (
-          <div className={styles.cartCard} key={item.id}>
+          <Link
+            to={`/product/${item.id}`}
+            className={styles.cartCard}
+            key={item.id}
+          >
             <img src={item.image.url} alt={item.image.alt} />
             <div className={styles.cartCardContent}>
-              <p>{item.title}</p>
+              <h5>{item.title}</h5>
               <p>Price: {item.discountedPrice} NOK</p>
               <p>Quantity: {item.quantity}</p>
               <button
                 className={buttonStyles.primaryButton}
                 onClick={() => handleRemoveCartItem(item.id)}
               >
-                Remove item
+                Remove
               </button>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
-      <div className={styles.totalSum}>Total Sum: {totalSum} NOK</div>    </div>
-  );
+      <div className={styles.totalSum}>Total Sum: {totalSum} NOK</div>{' '}
+    </div>
+  )
 }
