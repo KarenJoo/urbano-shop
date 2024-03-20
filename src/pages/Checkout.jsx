c09fd21891e0620cab4a0de18752fbdcf28fc1d4
Karen Jo <karenfiksdahl@gmail.com>
2024-03-20 21:40:27 +0100

    creates checkout page with link back to homepage, styles button and containers

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import styles from './checkout.module.css'
+import { Link } from 'react-router-dom'
+import buttonStyles from '../components/Buttons.module.css'
 
 export default function Checkout() {
   return (
     <div className='productContainer'>
-         <div>Checkout</div>
+      <div className={styles.checkoutContainer}>
+         <h1>Checkout</h1>
+         <h5>Your order was successful! Thank you for shopping at Urbano.</h5>
+         <Link
+            to={`/`}
+            className={buttonStyles.primaryButton}
+            
+          >
+            Continue shopping
+          </Link>
+          </div>
+        
     </div>
  
   )
