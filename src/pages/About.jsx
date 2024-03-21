cd47362cbf341a5f5e459665ddda125e121aa9f7
Karen Jo <karenfiksdahl@gmail.com>
2024-03-21 22:14:59 +0100

    adds main containers to theme

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import { useState } from 'react';
 
 export default function About() {
-  const [counter, setCounter] = useState(0);
 
-  function onButtonClick() {
-    setCounter(counter + 10);
-  }
   return (
-  <div>
-    <div>Counter: {counter}</div>
-    <button onClick={onButtonClick}>Add 10</button>
+  <div className='productContainer'>
+    <h1>About Urbano</h1>
   </div>
   );
 }
