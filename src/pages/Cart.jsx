6029caf08308bea0729012e9ce0263a05d52d623
Karen Jo <karenfiksdahl@gmail.com>
2024-03-22 19:30:42 +0100

    revert theme styling of container

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,73 +1,51 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { removeCartItem } from '../store/cartSlice'
-import buttonStyles from '../components/Buttons.module.css'
-import styles from './Cart.module.css'
-import { calculateCart } from '../store/cartCalculation'
-import { Link } from 'react-router-dom'
-import { clearCart } from '../store/cartSlice'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeCartItem } from '../store/cartSlice';
+import buttonStyles from '../components/Buttons.module.css';
+import styles from './Cart.module.css';
+import { calculateCart } from '../store/cartCalculation';
+import { Link } from 'react-router-dom';
+import { Grommet, Box } from 'grommet';
+import { theme } from '../theme';
 
 export default function CartPage() {
-  const cartItems = useSelector((state) => state.cart.cartItems)
-  const dispatch = useDispatch()
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const dispatch = useDispatch();
 
-  const totalSum = calculateCart(cartItems)
-  const totalQuantity = cartItems.reduce(
-    (total, item) => total + item.quantity,
-    0,
-  )
+  const totalSum = calculateCart(cartItems);
+  const totalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
 
   const handleRemoveCartItem = (id) => {
-    dispatch(removeCartItem({ id }))
-  }
-
-  const handleClearCart = () => {
-    dispatch(clearCart())
-    localStorage.removeItem('cartItems')
-  }
+    dispatch(removeCartItem({ id }));
+  };
 
   return (
-    <div className='productContainer'>
-      <div className={styles.cartContainer}>
-        <h2 className='headerTextDark'>Shopping Bag</h2>
-        <h3 className={styles.cartParagraph}>
-          {' '}
-          Products ({totalQuantity})
-        </h3>
-        {cartItems.map((item) => (
-          <Link
-            to={`/product/${item.id}`}
-            className={styles.cartCard}
-            key={item.id}
-          >
-            <img src={item.image.url} alt={item.image.alt} />
-            <div className={styles.cartCardContent}>
-              <h5>{item.title}</h5>
-              <p>Price: {item.discountedPrice} NOK</p>
-              <p>Quantity: {item.quantity}</p>
-              <button
-                className={buttonStyles.smallButton}
-                onClick={() => handleRemoveCartItem(item.id)}
-              >
-                Remove
-              </button>
-            </div>
-          </Link>
-        ))}
-        <div className={styles.totalSum}>
-          Total Sum: {totalSum} NOK{' '}
-          <Link to={`/`}>
-            <p>Shop more</p>
-          </Link>
-          <Link
-            to={`/checkout`}
-            className={buttonStyles.primaryButton}
-            onClick={handleClearCart}
-          >
-            Checkout
-          </Link>
+    <Grommet theme={theme}>
+     <Box fill="vertical" background="url('/assets/images/bg.jpg')">
+        <div style={theme.cartContainer}>
+          <h2>Shopping Bag</h2>
+          <h3>Products ({totalQuantity})</h3>
+          {cartItems.map((item) => (
+            <Link to={`/product/${item.id}`} className={styles.cartCard} key={item.id}>
+              <img src={item.image.url} alt={item.image.alt} />
+              <div className={styles.cartCardContent}>
+                <h5>{item.title}</h5>
+                <p>Price: {item.discountedPrice} NOK</p>
+                <p>Quantity: {item.quantity}</p>
+                <button className={buttonStyles.smallButton} onClick={() => handleRemoveCartItem(item.id)}>
+                  Remove
+                </button>
+              </div>
+            </Link>
+          ))}
+          <div className={styles.totalSum}>
+            Total Sum: {totalSum} NOK{' '}
+            <Link to={`/`}>
+              <p>Shop more</p>
+            </Link>
+          </div>
         </div>
-      </div>
-    </div>
-  )
+      </Box>
+    </Grommet>
+  );
 }
