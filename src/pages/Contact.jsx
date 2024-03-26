4acbb9c129e5918fd7c443761c8fc8046bf2d477
Karen Jo <karenfiksdahl@gmail.com>
2024-03-27 00:24:06 +0100

    adds error messages and styling  errors provided by react hook form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,45 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 import buttonStyles from '../components/Buttons.module.css'
 import styles from './Contact.module.css'
-import { useForm } from 'react-hook-form'
 
 export default function Contact() {
-  const { register, handleSubmit } = useForm()
   let firstName, lastName, email, subject, body
 
-  function onFormSubmit(data) {
-    data.preventDefault()
+  const schema = yup.object().shape({
+    firstName: yup
+      .string()
+      .min(3, 'First name must be at least 3 characters')
+      .max(10, 'First name cannot exceed 10 characters')
+      .required('First name is required'),
+    lastName: yup
+      .string()
+      .min(3, 'Last name must be at least 3 characters')
+      .max(10, 'Last name cannot exceed 10 characters')
+      .required('Last name is required'),
+    email: yup.string().email('Invalid email').required('Email is required'),
+    subject: yup
+      .string()
+      .min(3, 'Subject must be at least 3 characters')
+      .max(10, 'Subject cannot exceed 10 characters')
+      .required('Subject is required'),
+    body: yup
+      .string()
+      .min(10, 'Message must be at least 10 characters')
+      .max(200, 'Message cannot exceed 200 characters')
+      .required('Message is required'),
+  })
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) })
+
+  function onFormSubmit(data) {
+    console.log('Form submitted with:', data)
     console.log('Form submitted with:', {
       firstName,
       lastName,
@@ -38,6 +68,9 @@ export default function Contact() {
                   maxLength: 10,
                 })}
               />
+              {errors.firstName && (
+                <p className={styles.error}>{errors.firstName.message}</p>
+              )}
             </div>
             <div className={styles.nameContainer}>
               <label htmlFor='last-name'>Last name</label>
@@ -52,6 +85,9 @@ export default function Contact() {
                   maxLength: 10,
                 })}
               />
+              {errors.lastName && (
+                <p className={styles.error}>{errors.lastName.message}</p>
+              )}
             </div>
           </div>
           <label htmlFor='email'>Email</label>
@@ -63,16 +99,24 @@ export default function Contact() {
             placeholder='Email'
             {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
           />
-
+          {errors.email && (
+            <p className={styles.error}>{errors.email.message}</p>
+          )}
           <label htmlFor='subject'>Subject</label>
           <input
             id='subject'
             name='subject'
             value={subject}
             placeholder='Subject'
-            {...register('subject', { required: true })}
+            {...register('subject', {
+              required: true,
+              minLength: 3,
+              maxLength: 10,
+            })}
           />
-
+          {errors.subject && (
+            <p className={styles.error}>{errors.subject.message}</p>
+          )}
           <label htmlFor='body'>Message</label>
 
           <textarea
@@ -82,6 +126,7 @@ export default function Contact() {
             placeholder='Write here..'
             {...register('body', { required: true, min: 10, max: 200 })}
           />
+          {errors.body && <p className={styles.error}>{errors.body.message}</p>}
           <button type='submit' className={buttonStyles.primaryButton}>
             Submit
           </button>
