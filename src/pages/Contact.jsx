e74adcec198d22cb13c5c91a8ba8236afa71e01b
Karen Jo <karenfiksdahl@gmail.com>
2024-03-26 23:01:54 +0100

    adds input, label and useState to form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,30 +1,52 @@
-import React, { useState } from 'react';
-import buttonStyles from '../components/Buttons.module.css';
+import React, { useState } from 'react'
+import buttonStyles from '../components/Buttons.module.css'
+import styles from './Contact.module.css'
+import { useForm } from 'react-hook-form'
 
 export default function Contact() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const { register, handleSubmit } = useForm()
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [subject, setSubject] = useState('')
+  const [email, setEmail] = useState('')
+  const [body, setBody] = useState('')
 
   function onFirstNameChange(event) {
-    setFirstName(event.target.value);
+    setFirstName(event.target.value)
   }
 
   function onLastNameChange(event) {
-    setLastName(event.target.value);
+    setLastName(event.target.value)
+  }
+
+  function onEmailChange(event) {
+    setEmail(event.target.value)
+  }
+
+  function onSubjectChange(event) {
+    setSubject(event.target.value)
+  }
+
+  function onBodyChange(event) {
+    setBody(event.target.value)
   }
 
   function onFormSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
 
-    console.log('Form submitted with:', { firstName, lastName });
-
-    // You can add your form submission logic here
+    console.log('Form submitted with:', {
+      firstName,
+      lastName,
+      email,
+      subject,
+      body,
+    })
   }
 
   return (
     <div className='parentContainer'>
       <div className='childContainer'>
-        <form onSubmit={onFormSubmit}>
+        <form className={styles} onSubmit={onFormSubmit}>
           <h1>Contact form</h1>
           <label htmlFor='first-name'>First name</label>
           <input
@@ -42,11 +64,40 @@ export default function Contact() {
             placeholder='Your last name'
             onChange={onLastNameChange}
           />
+
+          <label htmlFor='email'>Email</label>
+
+          <input
+            id='email'
+            name='email'
+            value={email}
+            placeholder='Email'
+            onChange={onEmailChange}
+          />
+
+          <label htmlFor='subject'>Subject</label>
+          <input
+            id='subject'
+            name='subject'
+            value={subject}
+            placeholder='Subject'
+            onChange={onSubjectChange}
+          />
+
+          <label htmlFor='body'>Message</label>
+
+          <input
+            id='body'
+            name='body'
+            value={body}
+            placeholder='Write here..'
+            onChange={onBodyChange}
+          />
           <button type='submit' className={buttonStyles.primaryButton}>
             Submit
           </button>
         </form>
       </div>
     </div>
-  );
+  )
 }
