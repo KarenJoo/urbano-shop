e08248666fa3008c22db2aad4f78638a4f5344da
Karen Jo <karenfiksdahl@gmail.com>
2024-04-03 19:09:14 +0200

    adjusts styling on sale products and product cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,30 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './ProductCard.module.css';
-import OnSale from './OnSale';
-import styleButtons from '../components/Buttons.module.css';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './ProductCard.module.css'
+import OnSale from './OnSale'
+import styleButtons from '../components/Buttons.module.css'
 
 export default function ProductCard({ product }) {
-  const { id, image, title, description, price, discountedPrice } = product;
+  const { id, image, title, description, price, discountedPrice } = product
 
   return (
     <div className={styles.productCard}>
       <div className={styles.imageContainer}>
-        <img src={image.url} alt="Product Image" />
+        <img src={image.url} alt='Product Image' />
       </div>
       <div className={styles.cardContent}>
         <div className={styles.cardHeadContent}>
           <h2>{title}</h2>
-          <p>{description}</p> 
-         
+          <p>{description}</p>
         </div>
         <div className={styles.cardFootContent}>
-          <h4 className={price !== discountedPrice ? styles.onSale : ''}>{price} NOK</h4>
+          <h4 className={price !== discountedPrice ? styles.onSale : ''}>
+            {price} NOK
+          </h4>
           <OnSale price={price} discountedPrice={discountedPrice} />
-        </div>          
-        <Link to={`/product/${id}`} className={styleButtons.primaryButton}>View Product</Link>
-
+        </div>
+        <Link to={`/product/${id}`} className={styleButtons.primaryButton}>
+          View Product
+        </Link>
       </div>
     </div>
-  );
+  )
 }
