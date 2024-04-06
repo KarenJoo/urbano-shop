ee462713a48168d4fe839e75628ea152358a8cd0
Karen Jo <karenfiksdahl@gmail.com>
2024-04-06 09:58:56 +0200

    removes checkout module and styles in global css to resolve compiling issues

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from './Checkout.module.css'
 import { Link } from 'react-router-dom'
 import buttonStyles from '../components/Buttons.module.css'
 
@@ -7,16 +6,16 @@ export default function Checkout() {
   return (
     <div className='parentContainer'>
       <div className='childContainer'>
-        <div className={styles.checkoutContainer}>
-          <h1 className={styles.childHeader}>Checkout</h1>
-          <div className={styles.childText}>
+        <div className='childHeader'>
+          <h1>Checkout</h1>
+          <div>
             <p>Your order was successful!</p>
             <p>Thank you for shopping at Urbano.</p>
           </div>
-          <Link to={`/`} className={buttonStyles.primaryButton}>
-            Continue shopping
-          </Link>
         </div>
+        <Link to={`/`} className={buttonStyles.primaryButton}>
+          Continue shopping
+        </Link>
       </div>
     </div>
   )
